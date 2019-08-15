8d22cf878c988ebb163d44af3dc42aee042e41c3
Hugh Jarvis <hugh.jarvis@blueyonder.co.uk>
2019-08-15 11:30:10 +0100

    styling tweaks

--- src/containers/ComplimentContainer.js
+++ src/containers/ComplimentContainer.js
@@ -54,23 +54,24 @@ class ComplimentContainer extends Component {
         <div className="compliment_container">
           <div className="little_header_container">
           <img src="../../favicon.ico" alt="Big App Yourself logo"/>
-          <h1>Big App Yourself</h1>
+          <h1>Welcome to Big App Yourself</h1>
           </div>
 
 
 
           <div className="compliment">
-
-              <h1>Welcome to Big App Yourself!</h1>
-
             <h2>{this.state.compliment}</h2>
+
+            <div className="praise_button">
+              <button className="praise_me" onClick={this.handleBigAppButtonClick}><p><strong>Click for praise</strong></p></button>
+            </div>
+
           </div>
-          <div className="praise_button">
-            <button className="praise_me" onClick={this.handleBigAppButtonClick}><p><strong>Click for praise</strong></p></button>
-          </div>
+
+
 
           <form className="tweet-mate">
-            <label htmlFor="twitter_handle"><h3 className="tweet-spiel">Enjoy our compliments? Why not Big App a friend and share the love? Just enter your pal's Twitter handle below, click that blue button, and we'll compliment them for you.</h3></label>
+            <label htmlFor="twitter_handle"><h3 className="tweet-spiel">Enjoy our compliments? Why not Big App a friend and share the love? Just enter your pal's Twitter handle below, click the blue button, and we'll compliment them for you.</h3></label>
             <input className="twitter_handle" type="text" name="twitter_handle" placeholder="Enter a pal's Twitter handle here" onChange={this.handleFriend} value={this.state.friendsTwitter}/>
             <button className="send_tweet" onClick={this.handleTweetYourFriend}><p>Tweet my mate!</p></button>
           </form>
